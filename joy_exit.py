f8d8a6244648fc3faa4120659ec3544853124f99
battisti456 <battisti456@gmail.com>
2024-05-15 16:23:24 -0700

    added logging

--- joy_exit.py
+++ joy_exit.py
@@ -2,6 +2,22 @@ from pyjoystick.sdl2 import Key, Joystick, run_event_loop,stop_event_wait
 from time import time
 import subprocess
 import sys
+import logging
+
+LEVEL = logging.INFO
+
+logger = logging.getLogger()
+formatter = logging.Formatter("%(asctime)s::%(levelname)-8s::%(message)s")
+handlers:set[logging.Handler] = {
+    logging.FileHandler(
+        'joy_exit.log'
+    ),
+    logging.StreamHandler()
+}
+for handler in handlers:
+    handler.setFormatter(formatter)
+    handler.setLevel(LEVEL)
+    logger.addHandler(handler)
 
 class Exit(Exception):
     ...
@@ -23,26 +39,35 @@ def remove_handler(joystick:Joystick):
 def key_handler(key:Key):
     global start_holding, current_keys
     if key.keytype is Key.BUTTON:
+        logging.info(f"received: num = {key.number}, val = {key.value}")
         if key.value:#start pressing
             current_keys.add(key.number)
             if current_keys.issuperset(KEYS_TO_HOLD) and start_holding is None:
                 start_holding = time()
+                logging.warning(f"started holding")
         else:#stop pressing
             if key.number in current_keys:
                 current_keys.remove(key.number)
             if not start_holding is None:
-                if time() - start_holding > HOLD_TIME:
+                diff = time() - start_holding
+                if diff > HOLD_TIME:
+                    logging.warning(f"triggered exit at {diff} seconds of holding")
                     raise Exit()
             if key.number in KEYS_TO_HOLD:
+                logging.warning(f"canceled holding")
                 start_holding = None
 
         #print(f"{key.number} : {key.value}")
 
 if __name__ == '__main__':
+    logging.warning(f"starting child")
     process = subprocess.Popen(sys.argv[1:])
     try:
+        logging.info("starting to watch")
         run_event_loop(add_handler,remove_handler,key_handler)
     except Exit:
         ...
+    logging.info("killing child")
     process.kill()
+    logging.info("successful exit")
     
